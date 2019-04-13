8e3e3b0f98b83514943be8972746667667f0c6c3
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-04-12 23:18:26 -0300

    finish upload files in boxes

--- client/src/pages/Box/index.jsx
+++ client/src/pages/Box/index.jsx
@@ -5,40 +5,70 @@ import { MdInsertDriveFile } from 'react-icons/md';
 import api from '../../services/api';
 import { distanceInWords } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
 
 export default class Box extends Component {
   state = {
-    box: {}
+    box: {  }
   }
+
   async componentDidMount() {
     const box = this.props.match.params.id;
-    const response = await api.get(`boxes/${box}`);
+    const response = await api.get(`boxes/${ box }`);
 
     this.setState({ box: response.data });
+  }
+
+  handleUpload = (files) => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append('file', file);
+
+      api.post(`/boxes/${ box }/files`, data);
+    });
   }
 
   render() {
     return (
       <div id="box-container">
         <header>
-          <img src={logo} alt=""/>
-          <h1>{this.state.box.title}</h1>
+          <img src={ logo } alt=""/>
+          <h1>{ this.state.box.title }</h1>
         </header>
 
-        <ul>
-          { this.state.box.files && this.state.box.files.map(file => (
-            <li>
-              <a className="fileInfo" href={file.url} target="_blank" rel="noopener noreferrer">
-                <MdInsertDriveFile size={24} color="#ASCfff" />
-                <strong>{file.title}</strong>
-              </a>
+        <Dropzone onDropAccepted={ this.handleUpload }>
+          {
+            ({ getRootProps, getInputProps }) => (
+              <div className="upload" { ...getRootProps() }>
+                <input { ...getInputProps() }/>
 
-              <span>há {' '}
-              {distanceInWords(file.createdAt, new Date(), {
-                locale: pt
-              })}</span>
-            </li>
-          )) }
+                <p>Arraste arquivos ou clique aqui</p>
+              </div>
+            )
+          }
+        </Dropzone>
+
+        <ul>
+          {
+            this.state.box.files && this.state.box.files.map(file => (
+              <li key={ file._id }>
+                <a className="fileInfo" href={ file.url } target="_blank" rel="noopener noreferrer">
+                  <MdInsertDriveFile size={ 24 } color="#ASCfff" />
+                  <strong>{ file.title }</strong>
+                </a>
+
+                <span>há {' '}
+                {
+                  distanceInWords(file.createdAt, new Date(), {
+                    locale: pt
+                  })
+                }
+                </span>
+              </li>
+            ))
+          }
         </ul>
       </div>
     );
